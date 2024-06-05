095c041981255f848846e83df51f61faa759a374
thuyet <thuyet@ubuntu2.myguest.virtualbox.org>
2024-06-05 09:39:36 +0700

    Init synthapp project

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <juce_core/juce_core.h>
+
+int main (int argc, char* argv[])
+{
+
+    // Your code goes here!
+    juce::ignoreUnused (argc, argv);
+    std::cout << "synthapp hello world!\n";
+
+    return 0;
+}
