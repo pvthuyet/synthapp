5a8476ae17194bc0d3fb7330eec25c40d8dd2174
thuyet <thuyet@ubuntu2.myguest.virtualbox.org>
2024-06-06 07:41:07 +0700

    remove gui

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-#include "MainComponent.h"
+#include "PlayerManager.h"
 
 //==============================================================================
 class GuiAppApplication final : public juce::JUCEApplication
@@ -17,14 +17,14 @@ public:
     //==============================================================================
     void initialise (const juce::String& commandLine) override
     {
-        mainWindow.reset (new MainWindow (getApplicationName(), commandLine));
+        playerManager = std::make_unique<PlayerManager>(commandLine);
     }
 
     void shutdown() override
     {
         // Add your application's shutdown code here..
 
-        mainWindow = nullptr; // (deletes our window)
+        playerManager = nullptr; // (deletes our window)
     }
 
     //==============================================================================
@@ -43,54 +43,8 @@ public:
         juce::ignoreUnused (commandLine);
     }
 
-    //==============================================================================
-    /*
-        This class implements the desktop window that contains an instance of
-        our MainComponent class.
-    */
-    class MainWindow final : public juce::DocumentWindow
-    {
-    public:
-        explicit MainWindow (juce::String name, const juce::String& sfzFile)
-            : DocumentWindow (name,
-                              juce::Desktop::getInstance().getDefaultLookAndFeel()
-                                                          .findColour (ResizableWindow::backgroundColourId),
-                              DocumentWindow::allButtons)
-        {
-            setUsingNativeTitleBar (true);
-            setContentOwned (new MainComponent(sfzFile), true);
-
-           #if JUCE_IOS || JUCE_ANDROID
-            setFullScreen (true);
-           #else
-            setResizable (true, true);
-            centreWithSize (getWidth(), getHeight());
-           #endif
-
-            setVisible (true);
-        }
-
-        void closeButtonPressed() override
-        {
-            // This is called when the user tries to close this window. Here, we'll just
-            // ask the app to quit when this happens, but you can change this to do
-            // whatever you need.
-            JUCEApplication::getInstance()->systemRequestedQuit();
-        }
-
-        /* Note: Be careful if you override any DocumentWindow methods - the base
-           class uses a lot of them, so by overriding you might break its functionality.
-           It's best to do all your work in your content component instead, but if
-           you really have to override any DocumentWindow methods, make sure your
-           subclass also calls the superclass's method.
-        */
-
-    private:
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
-    };
-
 private:
-    std::unique_ptr<MainWindow> mainWindow;
+    std::unique_ptr<PlayerManager> playerManager;
 };
 
 //==============================================================================
---
+++ PlayerManager.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "JuceHeader.h"
+
+struct SynthAudioSource final : public AudioSource
+{
+    SynthAudioSource (MidiKeyboardState& keyState, const juce::String& sfzPath);
+    void loadFile(const juce::String& sfzPath);
+
+    void prepareToPlay (int /*samplesPerBlockExpected*/, double sampleRate) override;
+    void releaseResources() override {}
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+
+    // this collects real-time midi messages from the midi input device, and
+    // turns them into blocks that we can process in our audio callback
+    MidiMessageCollector midiCollector;
+
+    // this represents the state of which keys on our on-screen keyboard are held
+    // down. When the mouse is clicked on the keyboard component, this object also
+    // generates midi messages for this, which we can pass on to our synth.
+    MidiKeyboardState& keyboardState;
+
+    // the synth itself!
+    juce::AudioFormatManager formatManager;
+    sfzero::Synth synth;
+};
+
+//==============================================================================
+
+class Callback final : public AudioIODeviceCallback
+{
+public:
+    Callback (AudioSourcePlayer& playerIn);
+
+    void audioDeviceIOCallbackWithContext (const float* const* inputChannelData,
+                                           int numInputChannels,
+                                           float* const* outputChannelData,
+                                           int numOutputChannels,
+                                           int numSamples,
+                                           const AudioIODeviceCallbackContext& context) override;
+    void audioDeviceAboutToStart (AudioIODevice* device) override;
+    void audioDeviceStopped() override;
+
+private:
+    AudioSourcePlayer& player;
+};
+
+//==============================================================================
+
+class PlayerManager final
+{
+public:
+    PlayerManager(const juce::String& sfzFile);
+    ~PlayerManager();
+
+private:
+    juce::AudioDeviceManager audioDeviceManager;
+    juce::MidiKeyboardState keyboardState;
+    juce::AudioSourcePlayer audioSourcePlayer;
+    SynthAudioSource synthAudioSource;
+    Callback callback;
+};
