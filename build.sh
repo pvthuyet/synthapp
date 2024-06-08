c977d40ee2d69378993d2d2fce0dfbdb6061e076
thuyet <thuyet@toneworks.io>
2024-06-08 08:49:33 +0700

    change some config build

--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
-export LD_LIBRARY_PATH=/usr/lib/i386-linux-gnu
-
-echo "***************** 32 bit building ********************"
-cmake -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain-i386-linux-gnu.cmake -DBUILD_X86=ON -B outx86 -S .
-cmake --build outx86
-
-echo
-echo "***************** 64 bit building ********************"
-cmake -B outx64 -S .
-cmake --build outx64
+cmake -B build -S .
+cmake --build build --config Release --clean-first --target all
