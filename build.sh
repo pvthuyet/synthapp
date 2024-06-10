cc46f10afb5fdb7c02a7fa71201cf1fb6d540035
thuyet <thuyet@toneworks.io>
2024-06-11 06:52:29 +0700

    handle mindi event: ProgramChange, PitchWheel, Aftertouch, ChannelPressure

--- build.sh
+++ build.sh
@@ -1,9 +1,26 @@
-BUILD=$(pwd)/build/
+OUT=$(pwd)/out
+OUT_GUI=$(pwd)/out_gui
+
 if [ "$1" = "clean" ]; then
-    echo "Remove build folder: $BUILD"
-    rm -rf $BUILD
+    echo "Remove out folder: $OUT"
+    rm -rf $OUT
+    mkdir $OUT
+
+    rm -rf $OUT_GUI
+    mkdir $OUT_GUI
     echo "******************************"
 fi
 
-cmake -B $BUILD -S . -DCMAKE_BUILD_TYPE:STRING=Release
-cmake --build $BUILD --config Release --target synthapp --clean-first
+# build console
+echo "********* Console app building ..."
+cmake -B $OUT -S . -DCMAKE_BUILD_TYPE:STRING=Release
+cmake --build $OUT --config Release --target synthapp --clean-first
+
+# build gui
+echo "********* Gui app building ..."
+cmake -B $OUT_GUI -S . -DCMAKE_BUILD_TYPE:STRING=Release -DSHOW_GUI=1
+cmake --build $OUT_GUI --config Release --target synthapp --clean-first
+
+# copy to bin
+cp -f $OUT/synthapp_artefacts/Release/synthapp ./bin/synthapp
+cp -f $OUT_GUI/synthapp_artefacts/Release/synthapp ./bin/synthapp_gui
